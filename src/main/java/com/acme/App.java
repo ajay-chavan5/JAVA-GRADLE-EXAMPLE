f34c21a63dab5ff42a6828f47c2f8ee7463c88c9
Ajay Chavan <achavan@parkar.digital>
2024-06-27 16:54:39 +0530

    removed print statements

--- src/main/java/com/acme/App.java
+++ src/main/java/com/acme/App.java
@@ -3,12 +3,11 @@ package com.acme;
 // App
 public class App {
 
+  Logger log = logger.
+
   // main
   public static void main(String[] args) {
     App app = new App();
-    System.out.println(app.getGreeting());
-    System.out.println(app.getStatement());
-    System.out.println(app.getGoodbye());
   }
 
   // getGreeting
